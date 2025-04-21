422bb55e37334cc2893a0e2e5d47dc035132df9a
LindaW79 <135104328+LindaW79@users.noreply.github.com>
2025-04-21 21:41:10 +0200

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path"; // <-- lägg till
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // <-- viktigt
+    },
+  },
   build: {
     outDir: "../docs",
     emptyOutDir: true,
   },
 });
+
